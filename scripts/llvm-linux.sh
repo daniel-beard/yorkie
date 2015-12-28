f952d08218d5cc1b37cd4bb771ea157344f02ec6
daniel-beard <daniel.beard0@gmail.com>
2015-12-28 14:27:11 -0800

    .

--- scripts/llvm-linux.sh
+++ scripts/llvm-linux.sh
@@ -5,7 +5,5 @@ set -ev
 LLVM_TAR="clang+llvm-3.7.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
 
 curl -o $LLVM_TAR http://llvm.org/releases/3.7.0/$LLVM_TAR \
-    && mkdir -p vendor/llvm \
-    && tar -C vendor/llvm -xf $LLVM_TAR \
-    && ls && ls vendor/llvm \
-    && mv vendor/llvm/include vendor/llvm/Release/
+    && tar xf $LLVM_TAR \
+    && echo "Directory: " && ls
