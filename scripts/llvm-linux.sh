5b8589e2d7f23c7d1d20ac6b72129bf7adc83576
daniel-beard <daniel.beard0@gmail.com>
2015-12-28 14:08:18 -0800

    Change untar flags

--- scripts/llvm-linux.sh
+++ scripts/llvm-linux.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -ev
+
 LLVM_TAR="clang+llvm-3.7.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
 
 curl -o $LLVM_TAR http://llvm.org/releases/3.7.0/$LLVM_TAR \
     && mkdir -p vendor/llvm \
-    && tar -C vendor/llvm -xjf $LLVM_TAR \
+    && tar -C vendor/llvm -xf $LLVM_TAR \
     && mv vendor/llvm/include vendor/llvm/Release/
