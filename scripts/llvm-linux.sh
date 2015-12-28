0db8d8ca1d750bcf758b3421bcb3b04433587069
daniel-beard <daniel.beard0@gmail.com>
2015-12-28 14:12:00 -0800

    Add ls

--- scripts/llvm-linux.sh
+++ scripts/llvm-linux.sh
@@ -7,4 +7,5 @@ LLVM_TAR="clang+llvm-3.7.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz"
 curl -o $LLVM_TAR http://llvm.org/releases/3.7.0/$LLVM_TAR \
     && mkdir -p vendor/llvm \
     && tar -C vendor/llvm -xf $LLVM_TAR \
+    && ls && ls vendor/llvm \
     && mv vendor/llvm/include vendor/llvm/Release/
