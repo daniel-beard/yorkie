80f4ed994b59e4f6cc2d6a84755bdab2552eb09f
daniel-beard <daniel.beard0@gmail.com>
2016-03-20 14:06:59 -0700

    .

---
+++ scripts/osx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ev
+
+# http://llvm.org/releases/3.8.0/clang+llvm-3.8.0-x86_64-apple-darwin.tar.xz
+LLVM_VERSION="3.8.0"
+LLVM_FOLDER="clang+llvm-${LLVM_VERSION}-x86_64-apple-darwin"
+LLVM_TAR="${LLVM_FOLDER}.tar.xz"
+
+# Install cmake 3.5
+# CMAKE_FOLDER="cmake-3.5.0-Linux-x86_64"
+# curl -o cmake.tar.gz https://cmake.org/files/v3.5/"$CMAKE_FOLDER".tar.gz
+# tar xf cmake.tar.gz && mv "$CMAKE_FOLDER" cmake
+
+# Install Clang + LLVM
+curl -o $LLVM_TAR http://llvm.org/releases/${LLVM_VERSION}/${LLVM_TAR} \
+    && tar xf $LLVM_TAR \
+    && mv $LLVM_FOLDER llvm
